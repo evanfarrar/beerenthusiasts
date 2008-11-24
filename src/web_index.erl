5b66eb9d05f20424505ad5d5ef862a7e05c401e4
Tristan Sloughter <tristan@ir.iit.edu>
2008-11-24 15:32:15 -0600

    clean up

--- src/web_index.erl
+++ src/web_index.erl
@@ -2,6 +2,8 @@
 -include ("wf.inc").
 -export ([main/0, event/1]).
 
+%% Just learning... 
+
 main() ->
     Body = #body { body=#panel { style="margin: 50px;", 
                                  body=[#textbox { id=myTextbox1 },
@@ -12,7 +14,6 @@ main() ->
                                        #flash { id=flash },
                                        #button { text="Submit", postback=go },
                                        #panel { id=test }
-                                       %%"Hello there"2
                                       ]}},
     wf:render(Body).
 
@@ -21,11 +22,8 @@ event(go) ->
     wf:update (test, A);
 
 event(add) ->
-    %% A = wf:q(myTextbox),
     wf:flash(    [ #textbox { id=myTextbox1, postback=go },
                    #textbox { id=myTextbox2, postback=go },
                    #textbox { id=myTextbox3, postback=go } ]);
-%% wf:flash(A),
-    %%wf:update(test, "This is a test.");
 
 event(_) -> ok.
