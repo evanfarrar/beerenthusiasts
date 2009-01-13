ee876316475e0737cc656763e87cf3ecfbfab018
Tristan Sloughter <tristan@ir.iit.edu>
2009-01-13 10:56:27 -0600

    adding recipe_utils module

---
+++ src/utils/recipe_utils.erl
@@ -0,0 +1,35 @@
+%%% This file is part of beerenthusiasts.
+%%% 
+%%% beerenthusiasts is free software: you can redistribute it and/or modify
+%%% it under the terms of the GNU Affero General Public License as published by
+%%% the Free Software Foundation, either version 3 of the License, or
+%%% (at your option) any later version.
+%%% 
+%%% beerenthusiasts is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%% GNU Affero General Public License for more details.
+%%% 
+%%% You should have received a copy of the GNU Affero General Public License
+%%% along with beerenthusiasts.  If not, see <http://www.gnu.org/licenses/>.
+
+-module (recipe_utils).
+-include ("wf.inc").
+-export ([get_user_recipes/1, create_recipe/0, delete_recipe/0, get_recipe/0, save_recipe/0]).
+
+-include_lib ("stdlib/include/qlc.hrl").
+
+get_user_recipes (PageNum) ->
+    .
+
+create_recipe () ->
+    .
+
+delete_recipe () ->
+    .
+
+get_recipe () ->
+    .
+
+save_recipe () ->
+    .
