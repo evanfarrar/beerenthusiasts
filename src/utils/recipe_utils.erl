c6f5d333ed338ce47fd319b08087e1a9fb570f7a
Tristan Sloughter <tristan@kfgx61.(none)>
2009-01-13 13:18:18 -0600

    save_recipe implemented

--- src/utils/recipe_utils.erl
+++ src/utils/recipe_utils.erl
@@ -15,22 +15,42 @@
 
 -module (recipe_utils).
 -include ("wf.inc").
--export ([get_user_recipes/2, create_recipe/0, delete_recipe/0, get_recipe/0, save_recipe/0]).
+-export ([get_user_recipes/2, save_recipe/4, delete_recipe/0, get_recipe/1]).
 
 -include_lib ("stdlib/include/qlc.hrl").
 
+%%% Return recipes 10 at a time
 get_user_recipes (Username, PageNum) ->
     Options = [{count, 10}, {skip, integer_to_list(PageNum*10)}],
     couchdb_util:view_access("recipes", "user_recipes", Username, Options).
 
-create_recipe () ->
-    ok.
+save_recipe (ID, Username, RecipeName, Recipe) ->
+    JSON = {obj,
+            [{username, Username},
+             {name, RecipeName},
+             {ingredients, 
+              {obj, 
+               lists:foldr (fun (X, L) -> [create_json (X) | L] end, [], Recipe) }}]},
+    if 
+        ID =:= [] ->
+            couchdb_util:doc_create ("recipes", JSON);
+        true ->
+            couchdb_util:doc_create ("recipes", ID, JSON)
+    end.
 
 delete_recipe () ->
     ok.
 
-get_recipe () ->
-    ok.
+get_recipe (Name) ->
+    case rfc4627:decode(couchdb_util:doc_get ("recipes", Name)) of                
+        {error, Reason} ->
+            io:format (Reason),
+            [];
+        {ok, {obj, Doc}, _} -> 
+            Doc
+    end.    
 
-save_recipe () ->
-    ok.
+create_json ({Type, Type}) ->
+    {Type, list_to_binary(hd(wf:q(Type)))}; 
+create_json ({Type, List}) ->   
+    {list_to_binary(Type), [[list_to_binary(hd(wf:q(X))) || X <- L] || L <- List]}.
