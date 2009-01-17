7d2b0e34ae261f8876f9b06adda28a316eb3253f
Tristan Sloughter <tristan@kfgx61.(none)>
2009-01-17 15:16:18 -0600

    changed skip to integer

--- src/utils/recipe_utils.erl
+++ src/utils/recipe_utils.erl
@@ -16,13 +16,11 @@
 -module (recipe_utils).
 -include ("wf.inc").
 -include ("config.inc").
--export ([get_user_recipes/2, save_recipe/4, delete_recipe/0, get_recipe/1, create_user_recipe_view/1, create_recipe_comments_view/1, save_comment/5, save_profile/7]).
-
--include_lib ("stdlib/include/qlc.hrl").
+-export ([get_user_recipes/2, save_recipe/4, delete_recipe/0, get_recipe/1, create_user_recipe_view/1]).
 
 %%% Return recipes 10 at a time
 get_user_recipes (Username, PageNum) ->
-    Options = [{count, 10}, {skip, integer_to_list(PageNum*10)}],
+    Options = [{count, 10}, {skip, PageNum*10}],
     couchdb_utils:view_access(?COUCHDB_RECIPES_DB_NAME, "user_recipes", Username, Options).
 
 save_recipe (ID, Username, RecipeName, Recipe) ->
@@ -59,28 +57,3 @@ create_json ({Type, List}) ->
 create_user_recipe_view (Username) ->
     couchdb_utils:view_create (?COUCHDB_RECIPES_DB_NAME, Username, <<"javascript">>, [{all, list_to_binary("function(doc)\n{\n if (doc.username == \"" ++ Username ++ "\")\n {\n  emit(null, {\"id\": doc._id, \"name\":doc.name}); \n }\n}")}], []).
 
-create_recipe_comments_view (RecipeID) ->
-    couchdb_utils:view_create (?COUCHDB_COMMENTS_DB_NAME, RecipeID, <<"javascript">>, [{all, list_to_binary("function(doc)\n{\n if (doc.recipeid == \"" ++ RecipeID ++ "\")\n {\n  emit(null, doc); \n }\n}")}], []).
-
-save_comment (ID, RecipeID, Username, Title, Comment) ->
-    JSON = {obj,
-            [{recipeid, RecipeID},
-             {username, Username},
-             {title, Title},
-             {comment, Comment}]}, 
-    if 
-        ID =:= [] ->
-            couchdb_utils:doc_create (?COUCHDB_COMMENTS_DB_NAME, JSON);
-        true ->
-            couchdb_utils:doc_create (?COUCHDB_COMMENTS_DB_NAME, ID, JSON)
-    end.
-
-save_profile (Username, Name, Address, City, State, Zip, Bio) ->
-    JSON = {obj,
-            [{name, Name},
-             {address, Address},
-             {city, City},
-             {state, State},
-             {zip, Zip},
-             {bio, Bio}]}, 
-    couchdb_utils:doc_create (?COUCHDB_PROFILE_DB_NAME, Username, JSON).
